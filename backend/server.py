2262084abec17d6cdcc29f889e15a1af0a43483f
David Sanders <dsanders@rapilabs.com>
2017-08-10 03:49:57 +1000

    Setup serving of static files

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,11 @@
 from aiohttp import web
+from aiohttp_index import IndexMiddleware
 
 
 async def hello(request):
     return web.Response(text='Hello, world')
 
 
-app = web.Application()
-app.router.add_get('/', hello)
+app = web.Application(middlewares=(IndexMiddleware(),))
+app.router.add_static('/js', '../frontend/dist')
+app.router.add_static('/', '../frontend/public')
